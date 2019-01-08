a4cab90f919b637673609e073c5d9b138f99a9a0
Michel Hoche-Mong <hoche@grok.com>
2019-01-08 15:28:26 -0800

    Add a blocking submit method to the workqueue.

--- workqueue.hpp
+++ workqueue.hpp
@@ -1,3 +1,6 @@
+// Simple workqueue loosely adapted from stackexchange
+// https://codereview.stackexchange.com/questions/60363/thread-pool-worker-implementation
+
 #include <functional>
 #include <future>
 #include <deque>
@@ -8,6 +11,25 @@
 
 class WorkQueue {
 public:
+    // Initialize a workqueue with a requested number of workers.
+    //
+    // If numWorkers is less than 0, use the number of CPU threads available on the
+    // platform.
+    //
+    explicit WorkQueue(int numWorkers = -1)
+    {
+        if (numWorkers < 1) {
+            numWorkers = std::thread::hardware_concurrency();
+        }
+        if (numWorkers < 1) {
+            numWorkers = 1;
+        }
+
+        while (numWorkers--) {
+            m_workers.emplace_back(std::thread(&WorkQueue::doWork, this));
+        }
+    }
+
     // Initialize a workqueue with a worklist and a requested number of workers.
     //
     // We own the memory for the worklist after this.
@@ -15,7 +37,7 @@ public:
     // If numWorkers is less than 0, use the number of CPU threads available on the
     // platform.
     //
-    explicit WorkQueue(std::vector<std::function<void()>> &worklist, int numWorkers = -1) :
+    explicit WorkQueue(std::deque<std::function<void()>> &worklist, int numWorkers = -1) :
         m_work(worklist)
     {
         if (numWorkers < 1) {
@@ -39,7 +61,7 @@ public:
     void abort() {
         m_exit = true;
         m_finish_work = false;
-        m_signal.notify_all();
+        m_signalWaiting.notify_all();
 
         joinAll();
 
@@ -53,34 +75,58 @@ public:
     void waitForCompletion() {
         m_exit = true;
         m_finish_work = true;
-        m_signal.notify_all();
+        m_signalWaiting.notify_all();
 
         joinAll();
     }
 
+    void submit(std::function<void()> job, bool blocking = true) {
+        std::unique_lock<std::mutex> ul(m_mutex); // constructed locked
+        if (m_exit || !m_finish_work) {
+            return;
+        }
+        if (blocking && (m_work.size() >= m_workers.size())) {
+            // wait until a worker's available
+            //fprintf(stderr, "Blocking until a thread is available\n");
+            m_signalWorkDone.wait(ul);
+        }
+
+        // Ok to add and return;
+        //fprintf(stderr, "Adding job\n");
+        m_work.emplace_back(job);
+        m_signalWaiting.notify_one();
+    }
+
 private:
-    std::vector<std::function<void()>> m_work;
+    std::deque<std::function<void()>> m_work;
     std::vector<std::thread> m_workers;
-    int curItem{ 0 };
+    int thread_id = { 0 };
     std::mutex m_mutex;
-    std::condition_variable m_signal;
+
+    std::condition_variable m_signalWaiting;
+    std::condition_variable m_signalWorkDone;
+
     std::atomic<bool> m_exit{ false };
     std::atomic<bool> m_finish_work{ true };  // override m_exit until the work is done
 
     // Thread main loop
     void doWork() {
-        int endIdx = m_work.size();
         std::unique_lock<std::mutex> ul(m_mutex); // constructed locked
+        //int myId = thread_id++;
 
-        while (!m_exit || (m_finish_work && (curItem < endIdx))) {
-            if (curItem < endIdx) {
-                std::function<void()> work(std::move(m_work[curItem++]));
+        while (!m_exit || (m_finish_work && !m_work.empty()) ) {
+            if (!m_work.empty()) {
+                //fprintf(stderr, "    Thr[%d]: Working.\n", myId);
+                std::function<void()> work(std::move(m_work.front()));
+                m_work.pop_front();
                 ul.unlock();
                 work();
                 ul.lock();
-            }
-            else{
-                m_signal.wait(ul);
+                //fprintf(stderr, "    Thr[%d]: Done.\n", myId);
+                m_signalWorkDone.notify_one(); // This just notifies any blocking submits
+            } else {
+                //fprintf(stderr, "    Thr[%d]: Paused for more work.\n", myId);
+                m_signalWaiting.wait(ul);  // Wait until either new work is added or we're cleaning up
             }
         }
     }
